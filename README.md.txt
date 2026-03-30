ab0422696788b149d3113e6f9ac5bfa97ae5c2a1
Kajipuram Prince <princekajipuram@gmail.com>
2026-03-31 02:47:53 +0530

    Initial commit - database + papers

---
+++ README.md.txt
@@ -0,0 +1,21 @@
+# College PYQ Database
+
+## Description
+This project contains the database schema for storing previous year question papers.
+
+## Tables
+- Subject: Stores subject details
+- Paper: Stores exam papers with year and type
+
+## Features
+- Foreign key relationship between Subject and Paper
+- Unique constraint on (subject_code, year, exam_type)
+- Sample data included
+
+## How to Use
+1. Open MySQL Workbench
+2. Run the file college_pyq.sql
+3. Database and tables will be created automatically
+
+## Author
+Prince
---
+++ college_pyq.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE college_pyq;
+use college_pyq;
+
+CREATE TABLE Subject (
+    subject_code VARCHAR(20) PRIMARY KEY,
+    subject_name VARCHAR(100),
+    degree VARCHAR(50),
+    branch VARCHAR(50),
+    semester INT
+);
+
+CREATE TABLE Paper (
+    paper_id INT AUTO_INCREMENT PRIMARY KEY,
+    subject_code VARCHAR(20),
+    year INT,
+    exam_type VARCHAR(20),
+    pdf_path VARCHAR(255),
+    FOREIGN KEY (subject_code) REFERENCES Subject(subject_code)
+);
+
+INSERT INTO Subject (subject_code, subject_name, degree, branch, semester)
+VALUES
+('BECSE0O011','Data Mining and Warehousing','B.Tech','CSE',6),
+('BECSE3C014','Robotics and AI','B.Tech','CSE',6),
+('BECSE3C021','Big Data Analytics','B.Tech','CSE',6),
+('BECSE3C022','Deep Learning','B.Tech','CSE',6),
+('BECSE3C023','Blockchain','B.Tech','CSE',6);
+
+select * from subject;
+show tables;
+
+ALTER TABLE Paper
+ADD UNIQUE (subject_code, year, exam_type);
+
+ALTER TABLE Paper 
+MODIFY exam_type VARCHAR(20) CHECK (exam_type IN ('Mid','End'));
+SHOW INDEX FROM Paper;
+ALTER TABLE Paper DROP INDEX subject_code;
+SHOW INDEX FROM Paper;
+
+INSERT INTO Paper (subject_code, year, exam_type, pdf_path)
+VALUES 
+('BECSE3C022',2025,'Mid','papers/DL_2025_mid.pdf'),
+('BECSE3C022',2025,'End','papers/DL_2025_end.pdf'),
+('BECSE3C023',2025,'Mid','papers/BC_2025_mid.pdf'),
+('BECSE3C021',2025,'Mid','papers/BDA_2025_mid.pdf'),
+('BECSE3C021',2025,'End','papers/BDA_2025_end.pdf'),
+('BECSE0O011',2025,'Mid','papers/DMW_2025_mid.pdf'),
+('BECSE0O011',2025,'End','papers/DMW_2025_end.pdf'),
+('BECSE3C014',2025,'Mid','papers/rAI_2025_mid.pdf'),
+('BECSE3C014',2025,'End','papers/rAI_2025_end.pdf');
+
+SELECT subject_code, year, exam_type, pdf_path FROM Paper;
+
+
